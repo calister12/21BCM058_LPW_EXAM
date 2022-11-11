1bf250e0f70070c3e86f57af91c6fbeccac3b902
calister12 <115967564+calister12@users.noreply.github.com>
2022-11-11 15:09:07 +0530

    Update 21BCM058.c

--- 21BCM058.c
+++ 21BCM058.c
@@ -1,1 +1,12 @@
-#include<stdio.h>
+@a=();
+for($i=0;$i<6;$i++){
+    $t=<STDIN>;
+    push(@a,$t);
+}
+print(scalar @a."\n");
+print("Computer!!\n");
+for($i=0;$i<6;$i++){
+    if($a[$i]=~/BCE/){
+        print($a[$i]);
+    }
+}
